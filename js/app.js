3cdf70ef84dc1ca9ec646c6dc0e4c3debce21e8c
Jonathan DeFaveri <d.jonathan@gmail.com>
2015-08-10 22:26:54 -0400

    project plan

--- js/app.js
+++ js/app.js
@@ -12,6 +12,35 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	
+  	/*--- generate a random number between 1-100 ---*/
+
+
+  	/*--- make a guess ---*/
+  	//get value from text box
+  	//check value of input for validity
+  	//parse value into an integer 
+  	//add value to guess list 
+
+ 	/*--- provide feedback ---*/
+  	//bin value of input into hot, warm, cold, frigid range
+  	//insert feedback into feedback div
+
+  	
+  	/*--- New game reset ---*/
+  	$(".new").click(function(){
+  		alert('new game!');
+  		//newGame();
+  	});
+
 });
+
+function newGame () {
+	//reset the list of entered guesses
+	//reset the feedback
+	//reset the guess numbers to zero
+	//reset the guess input box
+	//generate a new random number
+}
 
 
