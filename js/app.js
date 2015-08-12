b9eff264690804ec91087f89124dcf381e04409c
Jonathan DeFaveri <d.jonathan@gmail.com>
2015-08-11 21:26:36 -0400

    guess input functionality added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
 	
+  	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,20 +13,43 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	//generate a random number between 1-100
+  	var answer=getRandomInt(1, 101)
+  	console.log('the answer is: '+answer); 
   	
-  	/*--- generate a random number between 1-100 ---*/
-
 
   	/*--- make a guess ---*/
-  	//get value from text box
-  	//check value of input for validity
-  	//parse value into an integer 
-  	//add value to guess list 
+  	$("#guessButton").click(function(){
 
- 	/*--- provide feedback ---*/
-  	//bin value of input into hot, warm, cold, frigid range
-  	//insert feedback into feedback div
 
+
+  		//get value from text box
+	  	var userGuessCount = 0
+	  	var userGuess = $('#userGuess').val()
+		//console.log(userGuess);
+	  	//console.log(typeof(userGuess));
+	  	//console.log(parseInt(userGuess));
+	  	userGuess = parseInt(userGuess)
+	  	//check value of input for validity (numeric btwn 1-100)
+	  	checkUserGuess(userGuess); 
+	  	//add value to guess list  ul#guessList
+	  	$("#guessList").append("<li>"+userGuess+"</li>");
+	  	//clear input field
+	  	$('#userGuess').val('');
+	  	//increase guess #count
+	  	userGuessCount++;
+	  	console.log(userGuessCount);
+	  	$("#count").text(userGuessCount);
+	  	
+
+	 	/*--- provide feedback for guess ---*/
+	  	//bin value of input into hot, warm, cold, frigid range
+	  	//insert feedback into #feedback div
+
+  	});
+
+  	/*--- provide feedback for relative guess ---*/
+  	//optional
   	
   	/*--- New game reset ---*/
   	$(".new").click(function(){
@@ -35,7 +59,26 @@ $(document).ready(function(){
 
 });
 
-function newGame () {
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function checkUserGuess(guess) {
+	if (guess > 100 || guess < 1) {
+		alert('Please enter a number between 1 and 100');
+	}
+	else if (isNaN(guess)) {
+		alert('Only numbers can be entered, please try again');
+	}
+
+	else {
+		return guess;
+	}	
+}
+
+function newGame() {
 	//reset the list of entered guesses
 	//reset the feedback
 	//reset the guess numbers to zero
